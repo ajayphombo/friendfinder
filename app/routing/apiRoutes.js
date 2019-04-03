a3cb6d6e8d8ca9c107423967405a1f4492137e00
ajayphombo <sagarsu@live.com>
2019-04-03 01:11:57 -0700

    some work in logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,21 +5,16 @@ module.exports = function(app) {
    
     app.get("/api/person", function(req, res) {
         res.json(personData);
+
+
+
+        
     });
 
     app.post("/api/friends", function(req, res) {
-        // req.body hosts is equal to the JSON post sent from the user
-        // This works because of our body parsing middleware
         var newperson = req.body;
-      
-        // Using a RegEx Pattern to remove spaces from newperson
-        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-        //newperson.routeName = newperson.name.replace(/\s+/g, "").toLowerCase();
-      
         console.log(newperson);
-      
         personData.push(newperson);
-      
         res.json(newperson);
       });      
 }; 
