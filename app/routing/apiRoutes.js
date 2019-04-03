bbca3563fdd099582473f61e7cde06c471a2e179
ajayphombo <sagarsu@live.com>
2019-04-02 22:23:24 -0700

    post method added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,6 +7,19 @@ module.exports = function(app) {
         res.json(personData);
     });
 
- 
+    app.post("/api/friends", function(req, res) {
+        // req.body hosts is equal to the JSON post sent from the user
+        // This works because of our body parsing middleware
+        var newperson = req.body;
       
+        // Using a RegEx Pattern to remove spaces from newperson
+        // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+        //newperson.routeName = newperson.name.replace(/\s+/g, "").toLowerCase();
+      
+        console.log(newperson);
+      
+        personData.push(newperson);
+      
+        res.json(newperson);
+      });      
 }; 
